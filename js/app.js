cf993342542f6c92be0608ff68edf3a3b7f278e1
a-yasinsky <anatoly@yasinsky.pro>
2017-11-08 11:42:29 +0600

    Add markers on the map

--- js/app.js
+++ js/app.js
@@ -2,23 +2,52 @@
 
 let map;
 
-function initMap() {
+function init() {
 	// Constructor creates a new map - only center and zoom are required.
 	map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: 41.087094, lng: -39.486305},
 	  zoom: 3
 	});
+	
+	ko.applyBindings(new ViewModel());
 }
 
 let City = function(data) {
+	let _self = this;
 	this.title = data.title;
 	this.location = data.location;
+	this.show = ko.observable(true);
+	this.marker = new google.maps.Marker({
+            position: _self.location,
+            title: _self.title,
+            animation: google.maps.Animation.DROP
+          });
 }
 
 let ViewModel = function() {
 	this.cities = ko.observableArray(cities.map(function(city){
 		return new City(city);
 	}));
+	
+	this.filteredCities = ko.computed(function() {
+		let bounds = new google.maps.LatLngBounds();
+		let filteredCities = this.cities().filter(function(city) {
+			if(city.show()) {
+				city.marker.setMap(map);
+				bounds.extend(city.marker.position);
+			}else {
+				city.marker.setMap(null);
+			}
+			return city.show();
+		});
+		map.fitBounds(bounds);
+		return filteredCities;
+	}.bind(this));
+	
+	this.filteredCities.subscribe(function(vaal){
+		console.log(vaal);
+	}, null, "change");
+	
+	//this.cities.push(new City({title: 'San Francisko', location: {lat: 37.752929, lng: -122.450649}}));
+	//this.cities()[0].show(false);
 }
-
-ko.applyBindings(new ViewModel());
